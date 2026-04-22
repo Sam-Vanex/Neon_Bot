520ea5300cd370aeabdd3f470425a9c6307a1421
Sam-Vanex <sam.vanex.be@gmail.com>
2026-04-23 01:45:10 +0200

    added temporary rules command

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import messages
 import discord
 from discord.ext import commands
 import logging
@@ -12,7 +13,25 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
+#sync the commands
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"Logged in as {bot.user}")
+
+@bot.tree.command(name="rules")
+async def rules_command(interaction: discord.Interaction):
+    await interaction.response.defer()
+    channel = interaction.channel
+    await channel.send(embeds=[
+        messages.Rules0,
+        messages.Rules1,
+        messages.Rules2,
+        messages.Rules3,
+        messages.Rules4,
+        messages.Rules5
+    ])
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
---
+++ messages.py
@@ -0,0 +1,37 @@
+import discord
+
+Rules0 = discord.Embed(
+    title="Rules and regulations 📃",
+    description="By being part of this server, you are required to follow all rules and guidelines. Breaking any rules may result in disciplinary action, including warnings, removal from channels, or a permanent ban if necessary.",
+    color=3066993
+)
+
+Rules1 = discord.Embed(
+    title="` Rule 1 ` Be Respectful",
+    description="Treat everyone with respect. Bullying, harassment, or targeted hate is not allowed. Light trash talk is tolerated as long as it stays friendly and doesn’t cross into disrespect or make others uncomfortable.",
+    color=2105893
+)
+
+Rules2 = discord.Embed(
+    title="` Rule 2 ` Keep Content Appropriate",
+    description="No NSFW (18+ content), explicit material, or disturbing content. This includes images, videos, usernames, or profile pictures.",
+    color=2105893
+)
+
+Rules3 = discord.Embed(
+    title="` Rule 3 ` Use Channels Properly",
+    description="Please use channels properly and stay on-topic. Each channel is made for a specific purpose, so keep messages relevant to avoid clutter and confusion.",
+    color=2105893
+)
+
+Rules4 = discord.Embed(
+    title="` Rule 4 ` No Excessive Pinging",
+    description="Excessive pinging is not allowed. This includes spamming mentions, repeatedly tagging users, or misusing @everyone/@here. Only use mentions when necessary and appropriate.",
+    color=2105893
+)
+
+Rules5 = discord.Embed(
+    title="` Rule 5 ` Follow Discord Terms of Service",
+    description="You must follow Discord’s Terms of Service and Community Guidelines. Any actions that violate Discord’s rules are not allowed here.",
+    color=2105893
+)
